def794315c18109599eed2b1aaed4d5e5c1ac647
NTeditor <nteditor4@gmail.com>
2025-06-23 03:40:33 +0300

    fix mason for termux

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "gopls", "lua_ls" },
-				automatic_installation = true,
+				automatic_installation = false
 			})
 		end
 	},
