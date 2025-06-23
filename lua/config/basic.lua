2c68d4527ed124f62896c928837365b2093006c3
NTeditor <nteditor4@gmail.com>
2025-06-23 07:50:57 +0300

    Изменено отображение ошибок

--- lua/config/basic.lua
+++ lua/config/basic.lua
@@ -9,5 +9,15 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	pattern = "*",
 	callback = function()
 		vim.diagnostic.open_float(nil, { focus = false })
-	end,
+	end
 })
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
+
+
