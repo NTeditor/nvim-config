3836c4f358b8d8c973b011f2786426191bbec296
NTeditor <nteditor4@gmail.com>
2025-06-23 22:30:26 +0300

    Добавленны keymap для управления tab

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,3 +1,9 @@
-vim.keymap.set("n", "<C-F>", function()
+local keymap = vim.keymap.set
+
+keymap("n", "<C-F>", function()
 	vim.lsp.buf.format({ async = true })
 end, { desc = "LSP: Format" })
+
+keymap("n", "<leader>tn", ":tabnew ", { desc = "New tab", noremap = true })
+keymap("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
+keymap("n", "<leader>to", ":tabonly<CR>", { desc = "Close all but current" })
