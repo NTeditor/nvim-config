b158e1c9157be45a7dd0b1ee497893dc98b71d99
nteditor <nteditor4@gmail.com>
2025-07-25 02:28:54 +0300

    Форматирован код

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,6 +1,7 @@
 local treeApi = require('nvim-tree.api')
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>b', treeApi.tree.toggle, { desc = "" })
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = "" })
 vim.keymap.set('n', '<leader>qq', ':q<CR>', { desc = "" })
 vim.keymap.set('n', '<leader>qf', ':q!<CR>', { desc = "" })
 vim.keymap.set('n', '<leader>wq', ':wq<CR>', { desc = "" })
